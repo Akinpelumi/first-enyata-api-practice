573dfaf757e62bdf84ecabdf90570200a3681bf7
Akinpelumi <akintundeakinpelumi@gmail.com>
2020-03-03 12:26:30 +0100

    latest

--- models/user.js
+++
@@ -1,8 +0,0 @@
-export default class User{
-    constructor(options){
-        this.first_name = options.first_name;
-        this.last_name = options.last_name;
-        this.email = options.email;
-        this.plainPassword = options.password
-    }
-}
